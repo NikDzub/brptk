a2e47def5025acf9d119de16106f3f329a2846a9
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-04-26 18:25:54 +0300

    .

--- open_url.py
+++ open_url.py
@@ -13,20 +13,19 @@ serial = sys.argv[1]  # 127.0.0.1:6562
 
 author_id = "6923224065499694085"
 aweme_id = "7352165286990171424"
-d = "ed672222h8l334"
 cid = "7362099827268993800"
 uid = "7255177357119161349"
 
-url = f"""
-https://www.tiktok.com/@{author_id}/
-video/{aweme_id}?
-_d={d}&
-comment_author_id={uid}&
-share_comment_id={cid}&
-""".replace(
-    "\n", ""
-)
+# url = f"""
+# https://www.tiktok.com/@{author_id}/
+# video/{aweme_id}?
+# comment_author_id={uid}&
+# share_comment_id={cid}&
+# """.replace(
+#     "\n", ""
+# )
 
+url = "https://www.tiktok.com/@6923224065499694085/video/7352165286990171424?comment_author_id=7255177357119161349&share_comment_id=7362149438363976466"
 print(url)
 
 
