d48bc9c3dfe325d894cd7fa9b6a3d7a61b6a0642
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-08-11 21:01:03 +0300

    .

--- 01_search_vids_loop.sh
+++ 01_search_vids_loop.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S")
+BRWSRS=$1
+
 
 # echo "Start: $CURRENT_TIME"
 
 CURRENT_DIR=$(pwd)
 
+
 while true; do
     echo "search videos loop started: $CURRENT_TIME"
-    python3 $CURRENT_DIR/01_get_vids_loop/in_loop.py 2 # (# brwsrs) (# new vids)
+    python3 "$CURRENT_DIR/01_get_vids_loop/in_loop.py" "$BRWSRS"
     if [ $? -ne 0 ]; then
         echo "failed, restarting..."
         continue
